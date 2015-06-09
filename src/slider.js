f05a689524816fd1a2e18d51bd2953aae7b417e1
unbalancedpaarentheses <federico.carrone@gmail.com>
2015-06-09 09:11:37 -0300

    added support for window resize

--- src/slider.js
+++ src/slider.js
@@ -4,9 +4,18 @@ import React from "react";
 
 class Slider extends React.Component{
 
-    constructor(props) {
+    constructor (props) {
         super(props);
+
+        this.state = {
+            width: 0
+        };
+
         this.onKeyDown = this.onKeyDown.bind(this);
+        this.updateDimensions = this.updateDimensions.bind(this);
+        this.componentWillMount = this.componentWillMount.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentWillUnmount = this.componentWillUnmount.bind(this);
     }
 
     onKeyDown (event) {
@@ -46,15 +55,41 @@ class Slider extends React.Component{
         this.props.playVideo(videoClickN);
     }
 
+    updateDimensions () {
+        this.setState({width: window.innerWidth});
+    }
+
+    componentWillMount () {
+        this.updateDimensions();
+    }
+
+    componentDidMount () {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount () {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
     render () {
+        let numberOfVideos;
+
+        if (this.state.width < 640) {
+            numberOfVideos = 2;
+        } else if (this.state.width < 1024) {
+            numberOfVideos = 3;
+        } else {
+            numberOfVideos = 5;
+        }
+
         let videoN = this.props.videoN;
 
-        let firstVideo = 5 * Math.floor(videoN / 5);
+        let firstVideo = numberOfVideos * Math.floor(videoN / numberOfVideos);
 
         let styles = [];
         let i;
 
-        for (i = firstVideo; i < firstVideo + 5; i++) {
+        for (i = firstVideo; i < firstVideo + numberOfVideos; i++) {
             let thumb;
 
             if (this.props.videos[0]) {
@@ -67,7 +102,7 @@ class Slider extends React.Component{
                 backgroundSize: "cover",
                 backgroundImage: "url(" + thumb + ")",
                 height: "100%",
-                width: "20%",
+                width: "calc(100% / " + numberOfVideos + ")",
                 float: "left"
             };
 
